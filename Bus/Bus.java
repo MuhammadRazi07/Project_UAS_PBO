b6ba73706bcd6de45bfa998ae16313a09138aa6d
MuhammadRazi07 <muhammadrazi072006@gmail.com>
2025-12-01 14:25:01 +0700

    Bus

--- Bus/Bus.java
+++ Bus/Bus.java
@@ -16,3 +16,46 @@ public class Bus {
         }
         return false;
     }
+
+    public boolean naik(Penumpang p) {
+
+        if (penumpang.size() >= kapasitas) {
+            System.out.println("❌ Bus penuh!");
+            return false;
+        }
+
+        try {
+            p.bayarOngkos(ONGKOS);
+        } catch (Exception e) {
+            System.out.println("❌ " + e.getMessage());
+            return false;
+        }
+
+        penumpang.add(p);
+        System.out.println(" Penumpang berhasil naik!");
+        return true;
+    }
+
+    public boolean turun(int id) {
+        for (Penumpang p : penumpang) {
+            if (p.getID() == id) {
+                penumpang.remove(p);
+                System.out.println("✔ Penumpang ID " + id + " telah turun.");
+                return true;
+            }
+        }
+        System.out.println("❌ Penumpang dengan ID itu tidak ada.");
+        return false;
+    }
+
+    public void printPenumpang() {
+        System.out.println("\n===== DAFTAR PENUMPANG =====");
+        if (penumpang.isEmpty()) {
+            System.out.println("Bus masih kosong.");
+            return;
+        }
+        for (Penumpang p : penumpang) {
+            System.out.println(p);
+        }
+    }
+}
