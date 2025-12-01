3e78a048e0c04156e943d1a602eaee43f7c14368
MuhammadRazi07 <muhammadrazi072006@gmail.com>
2025-12-01 14:51:26 +0700

    Test Bus

--- Bus/TestBus.java
+++ Bus/TestBus.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class TestBus {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        Bus bus = new Bus(10);
+
+        int menu;
+
+        do {
+            System.out.println("\n===== BUS TRANS KOETARADJA ======");
+            System.out.println("MENU :");
+            System.out.println("1. Naikkan Penumpang");
+            System.out.println("2. Turunkan Penumpang");
+            System.out.println("3. Lihat Penumpang");
+            System.out.println("4. Keluar");
+            System.out.print("Pilih menu: ");
+            menu = scan.nextInt();
