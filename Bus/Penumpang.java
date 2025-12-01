53a58c4bc1ec002e9a6d264218c8e7442de0189e
MuhammadRazi07 <muhammadrazi072006@gmail.com>
2025-12-01 10:13:25 +0700

    Penumpang

--- Bus/Penumpang.java
+++ Bus/Penumpang.java
@@ -1,1 +1,15 @@
+public class Penumpang implements Bayar {
+
+    protected int id;
+    protected int umur;
+    protected boolean hamil;
+    protected int saldo;
+    protected TipePenumpang tipe = TipePenumpang.BIASA;
+
+    public Penumpang(int id, int umur, boolean hamil) {
+        this.id = id;
+        this.umur = umur;
+        this.hamil = hamil;
+        this.saldo = 10000;
+    }
 
