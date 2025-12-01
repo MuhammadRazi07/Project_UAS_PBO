47040d5b18895349f702e494d761ea771ad8adce
MuhammadRazi07 <muhammadrazi072006@gmail.com>
2025-12-01 13:16:14 +0700

    bus

--- Bus/Bus.java
+++ Bus/Bus.java
@@ -1,1 +1,18 @@
+import java.util.ArrayList;
 
+public class Bus {
+
+    private ArrayList<Penumpang> penumpang = new ArrayList<>();
+    private int kapasitas;
+    public static final int ONGKOS = 2000;
+
+    public Bus(int kapasitas) {
+        this.kapasitas = kapasitas;
+    }
+
+    public boolean idAda(int id) {
+        for (Penumpang p : penumpang) {
+            if (p.getID() == id) return true;
+        }
+        return false;
+    }
