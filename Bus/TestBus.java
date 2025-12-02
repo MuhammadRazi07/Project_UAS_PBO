da8f338bcd2e251343c582ed6f00bdf42f6b7e61
MuhammadRazi07 <muhammadrazi072006@gmail.com>
2025-12-02 11:42:33 +0700

    Test Bus

--- Bus/TestBus.java
+++ Bus/TestBus.java
@@ -17,3 +17,54 @@ public class TestBus {
             System.out.println("4. Keluar");
             System.out.print("Pilih menu: ");
             menu = scan.nextInt();
+
+            switch (menu) {
+
+                case 1:
+                    System.out.print("Masukkan ID Penumpang: ");
+                    int id = scan.nextInt();
+
+                    if (bus.idAda(id)) {
+                        System.out.println("❌ ID sudah digunakan penumpang lain!");
+                        break;
+                    }
+
+                    System.out.print("Masukkan umur: ");
+                    int umur = scan.nextInt();
+
+                    System.out.print("Apakah hamil? (true/false): ");
+                    boolean hamil = scan.nextBoolean();
+
+                    Penumpang p;
+                    if (umur > 60 || hamil) {
+                        p = new PenumpangPrioritas(id, umur, hamil);
+                    } else {
+                        p = new Penumpang(id, umur, hamil);
+                    }
+
+                    bus.naik(p);
+                    break;
+
+                case 2:
+                    System.out.print("Masukkan ID penumpang yang ingin turun: ");
+                    int turunID = scan.nextInt();
+                    bus.turun(turunID);
+                    break;
+
+                case 3:
+                    bus.printPenumpang();
+                    break;
+
+                case 4:
+                    System.out.println("Keluar dari program.");
+                    break;
+
+                default:
+                    System.out.println("Menu tidak valid!");
+            }
+            
+        } while (menu != 4);
+
+        scan.close();
+    }
+}
