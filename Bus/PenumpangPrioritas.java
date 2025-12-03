f5219f0ff56fe21a63c1ff7a9b0e29c0899008d7
MuhammadRazi07 <muhammadrazi072006@gmail.com>
2025-12-03 20:35:18 +0700

    Penumpang Prioritas

--- Bus/PenumpangPrioritas.java
+++ Bus/PenumpangPrioritas.java
@@ -4,3 +4,17 @@ public class PenumpangPrioritas extends Penumpang {
         super(id, umur, hamil);
         this.tipe = TipePenumpang.PRIORITAS;
     }
+
+    @Override
+    public void bayarOngkos(int ongkos) throws Exception {
+        if (saldo < ongkos) {
+            throw new Exception("Saldo penumpang prioritas tidak cukup!");
+        }
+        saldo -= ongkos;
+    }
+
+    @Override
+    public String toString() {
+        return "[PRIORITAS] " + id + " (umur: " + umur + ", hamil: " + hamil + ", saldo: " + saldo + ")";
+    }
+}
